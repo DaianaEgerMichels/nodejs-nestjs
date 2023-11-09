305277d740368d68056d1049d267acf8a90d33d4
Daiana Michels <daiana.michels@pixeon.com>
2023-11-09 09:12:36 -0300

    added product dtos and validations

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Post, Get, Body } from '@nestjs/common';
 import { ProductRepository } from './product.repository';
+import { ProductCreateDTO } from './dto/ProductCreate.dto';
 
 @Controller('/products')
 export class ProductController {
   constructor(private productRepository: ProductRepository) {}
 
   @Post()
-  async createProduct(@Body() dataProduct) {
+  async createProduct(@Body() dataProduct: ProductCreateDTO) {
     this.productRepository.save(dataProduct);
     return dataProduct;
   }
--- src/user/dto/UserCreate.dto.ts
+++ src/user/dto/UserCreate.dto.ts
@@ -1,12 +1,12 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class UserCreateDTO {
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Name is required, it cannot be empty' })
   name: string;
 
-  @IsEmail()
+  @IsEmail(undefined, { message: 'The email provided is not valid' })
   email: string;
 
-  @MinLength(6)
+  @MinLength(6, { message: 'Password must have at least 6 characters' })
   password: string;
 }
